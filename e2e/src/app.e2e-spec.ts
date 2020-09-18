960c2925d2fe2abec8fc83c5cd78fd6a49c75d47
diegominelli <diego.minelli@hotmail.com>
2020-09-18 15:54:55 -0300

    Inicialização do projeto Gerenciador de tarefas

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,9 @@
+import { GerenciadorDeTarefasPage } from './app.po';
+
+describe('gerenciar-de-tarefas App', () => {
+  let page: GerenciadorDeTarefasPage;
+
+  beforeEach(() => {
+    page = new GerenciadorDeTarefasPage();
+  });
+});
