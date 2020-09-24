1c82af54791c45f92855c94c90f4e7bf8463300b
diegominelli <diego.minelli@hotmail.com>
2020-09-23 21:05:00 -0300

    Implementando a diretiva de tarefa concluida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TarefasModule } from './tarefas';
-import { TarefaConcluidaDirective } from './tarefas/shared/tarefa-concluida.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TarefaConcluidaDirective,
   ],
   imports: [
     BrowserModule,
